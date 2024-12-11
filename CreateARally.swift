d854dbd0e210b9efa12487c0bf258202f1c23225
Flaminkko <olizdu123@gmail.com>
2024-12-11 10:56:34 -0600

    created a new file and view called CreateARally

---
+++ CreateARally.swift
@@ -0,0 +1,14 @@
+//
+//  CreateARally.swift
+//  RallyRecon
+//
+//  Created by Olivier Zdunczyk on 12/11/24.
+//
+
+import SwiftUI
+
+struct CreateARally: View {
+    var body: some View {
+        Text("Placeholder")
+    }
+}
