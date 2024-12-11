e1c3a3711d540858dd4e5133534c04bd581c6e4f
Amar <asetka7368@stu.d214.org>
2024-12-11 11:02:32 -0600

    added the background and the title name

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
+            VStack {
+                Text("RallyRecon")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                    .font(.system(size:30, weight: .bold))
+            }
         }
     }
 }
