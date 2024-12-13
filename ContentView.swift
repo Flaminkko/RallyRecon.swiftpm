24846341d9c0dc0ef21eb1727dc8737c2adf5118
Ryan Strass <rstrass7397@stu.d214.org>
2024-12-13 11:15:00 -0600

    helped to fix blockage

--- ContentView.swift
+++ ContentView.swift
@@ -2,15 +2,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .ignoresSafeArea()
-            VStack {
-                Text("RallyRecon")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                    .font(.system(size:90, weight: .bold))
-                    .padding(30)
+        NavigationStack{
+            ZStack{
+                Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .ignoresSafeArea()
+                VStack{
+                    Text("RallyRecon")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                        .font(.system(size:90, weight: .bold))
+                        .padding(30)
+                    NavigationLink("Saved Rallies", destination: SavedRallys())
+                }
+                }
             }
         }
     }
-}
