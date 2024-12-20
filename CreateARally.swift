5ff62b5c3d784d022acc470c8a08d9c5a69f4a71
Amar <asetka7368@stu.d214.org>
2024-12-20 09:27:25 -0600

    changed logo size

--- CreateARally.swift
+++ CreateARally.swift
@@ -8,7 +8,18 @@
 import SwiftUI
 
 struct CreateARally: View {
+    @State var RallyName = ""
     var body: some View {
-        Text("Placeholder!")
+        ZStack{
+            Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
+            VStack{
+                TextField("Enter Stage Name", text: $RallyName)
+                    .padding()
+                    .textFieldStyle(.roundedBorder)
+                    .foregroundColor(.black)
+            }
+        }
     }
 }
