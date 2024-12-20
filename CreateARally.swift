c7fa28912cfe7ac1faab4b8982a5f246323510eb
Ryan Strass <rstrass7397@stu.d214.org>
2024-12-20 09:24:12 -0600

    added next button to create a rally

--- CreateARally.swift
+++ CreateARally.swift
@@ -10,5 +10,12 @@ import SwiftUI
 struct CreateARally: View {
     var body: some View {
         Text("Placeholder!")
+        NavigationLink("Next", destination: TurnInfoMod())
+            .frame(width: 300, height: 80)
+            .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
+            .cornerRadius(20)
+            .position(x: 410, y: 80)
+            .font(.system(size:40, weight: .bold))
+            .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
     }
 }
