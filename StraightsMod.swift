3db73aaabf4dfbce20cb8e9346edc502d9a92e13
Flaminkko <olizdu123@gmail.com>
2025-01-14 11:10:46 -0600

    added a next button that takes you to crests mod (will prolly be changed later)

--- StraightsMod.swift
+++ StraightsMod.swift
@@ -76,6 +76,15 @@ struct StraightsMod: View {
                     .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueStraights["Into"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
             }
         }
+        HStack{
+            NavigationLink("Next", destination: CrestsMod())
+                .frame(width: 300, height: 80)
+                .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
+                .cornerRadius(20)
+                .font(.system(size:40, weight: .bold))
+                .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
+                .padding(60)
+        }
     }
 }
 
