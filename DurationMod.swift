4e0e17c2c64b54c0211cfe900ded544d426aee60
Ryan Strass <rstrass7397@stu.d214.org>
2025-01-14 10:44:38 -0600

    worked on one of the buttons for duration mod

--- DurationMod.swift
+++ DurationMod.swift
@@ -10,7 +10,21 @@ import SwiftUI
 struct DurationMod : View {
     @State var isTrueDuration = ["short" : false, "long" : false, "very long" : false, "extra long" : false, "extremely long" : false, "late" : false, "tightens" : false, "opens" : false]
     var body: some View {
-        Text("PlaceHolder")
+        HStack{
+            Button {
+                isTrueDuration["short"]!.toggle()
+                isTrueDuration["long"] = false
+                isTrueDuration["very long"] = false
+                isTrueDuration["extra long"] = false
+            } label: {
+                Text("Short")
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(.white)
+                    .font(.system(size:60, weight: .bold))
+                    .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueDuration["short"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                    .position(x: 215, y: 450)
+            }
+        }
     }
 }
 
