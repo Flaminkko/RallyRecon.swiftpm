1355fdf7d72ba21c7f215be5894f44544b9da047
Amar <asetka7368@stu.d214.org>
2025-01-23 10:48:27 -0600

    I made a text view in saved rallies and saved tracks

--- SavedTracks.swift
+++ SavedTracks.swift
@@ -1,20 +1,30 @@
+//
+//  SavedTracks.swift
+//  RallyRecon
+//
+//  Created by Amar Setka on 1/23/25.
+//
+
 import SwiftUI
 
-struct SavedRallys : View {
+struct SavedTracks : View {
     var body: some View {
         ZStack{
             Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .ignoresSafeArea()
             VStack{
-                Text("Saved Rallies")
+                Text("Saved Tracks")
                     .font(.system(size:90, weight: .bold))
                     .padding(30)
                     .foregroundColor(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
             }
         }
+        
     }
 }
 #Preview {
-    SavedRallys()
+    SavedTracks()
 }
+
+
